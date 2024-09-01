274d002ecbdab78690e655330510286ee4dd6cf2
AntonioMadureira22 <you@example.com>
2024-08-31 20:58:03 -0400

    put together the bot responses

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Chatbot() {
+const responses = {
+    'hello': 'Hi! I am Chat Bot. How can I be of assistance?',
+    'how are you':'I am just a simple bot, I do not have emotions but maybe one day!',
+    'can you do math' : '9 + 10 = 21',
+    'default' : 'I am not sure how to respond to that. My capabilities are limited at the moment'
+};
+
+const Chatbot = () => {
+    const[messages, setMessages] = useState([]);
+    const [input, setInput] = useState('');
+    const handleSendMessage = () => {
+        if (input.trim()) {
+            setMessages([...messages, { text: input, isBot: false}]);
+            setInput('');
+            const botResponse = responses[input.toLowerCase()] ||['default'];
+            setMessages([...messages, {text: input, isBot: false}, {text: botResponse, isBot:true}]);
+        }
+    }
   return (
     <div>Chatbot</div>
   )
